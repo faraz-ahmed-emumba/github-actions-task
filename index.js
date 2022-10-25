6c60cb65287bc204a1ad5ac1a8d8034eddcf5753
Syed Faraz Ahmed <faraz.ahmed@emumba.com>
2022-10-26 00:05:09 +0500

    Added deploy stage and changed runs-on to self-hosted

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello from Syed Faraz Ahmed!");
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end("This webserver is serving app version "+process.env.APP_VERSION+"\nAuthor: "+process.env.NAME);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`The server is running on http://${hostname}:${port}/`);
+});
